94a5bee2d647844e10d7d61a1b9936a974b4b594
jnragu <40606827+jnragu@users.noreply.github.com>
2021-07-07 22:05:26 -0400

    cleaned up some crap

--- edchat/src/config.js
+++
@@ -1,4 +0,0 @@
-import { projects } from 'json!../.firebaserc'
-
-export const apiKey = "AIzaSyAnnZxuCx8dSwMCFzop8YqkVX96ZdTYwg8"
-export const projectId = projects.default
--- edchat/src/index.js
+++ edchat/src/index.js
@@ -1,47 +1,17 @@
-import refireApp, { firebase } from 'refire-app'
-import injectTapEventPlugin from 'react-tap-event-plugin'
-import { momentLocaleSetup } from './utils'
-import 'native-promise-only'
-injectTapEventPlugin()
-momentLocaleSetup()
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// import elemental css
-import '../node_modules/elemental/less/elemental.less'
-// highlight.js
-import '../node_modules/highlight.js/styles/default.css'
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-import { userReducer } from './reducers'
-
-import { apiKey, projectId } from './config'
-import bindings from './bindings'
-import routes from './routes'
-
-refireApp({
-  apiKey,
-  projectId,
-  bindings,
-  routes,
-  reducers: {
-    authenticatedUser: userReducer,
-  },
-  pathParams: (state) => state.routing.params,
-  onAuth: (authData, ref) => {
-    // update users/:uid with latest user data after successful authentication
-    if (authData && authData.uid) {
-      const { uid, providerId, displayName, photoURL, email } = authData
-      ref.child(`users/${uid}`).update({
-        provider: providerId,
-        displayName,
-        profileImageURL: photoURL,
-        lastLoginAt: firebase.database.ServerValue.TIMESTAMP,
-        email: email,
-      })
-      // set registeredAt to current timestamp if this is the first login
-      ref.child(`users/${uid}/registeredAt`).transaction((registeredAt) => {
-        if (!registeredAt) {
-          return firebase.database.ServerValue.TIMESTAMP
-        }
-      })
-    }
-  },
-})
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
