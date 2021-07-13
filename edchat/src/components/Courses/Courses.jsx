61a40074b4750e9b10812989ef0176af4484f2de
Diana Rosado <drosado1215@gmail.com>
2021-07-12 22:23:28 -0400

    side bar

--- edchat/src/components/Courses/Courses.jsx
+++ edchat/src/components/Courses/Courses.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core';
+import React, { useState } from 'react';
+import { makeStyles, Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
 import courseStyle from './coursesStyle.js'
-
+import PersonAddOutlinedIcon from '@material-ui/icons/PersonAddOutlined';
 const useStyles = makeStyles(courseStyle);
 
 function Courses() {
     const classes = useStyles()
+
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     const courses = [
         {
@@ -36,10 +46,42 @@ function Courses() {
             <p style={{ textAlign: 'center', color: '#A3A3A3', marginBottom: '30px', fontSize: '13px', }}>Current courses ({courses.length})</p>
             {courses.map((course, index) => (
                 <div key={index} className={classes.courses}>
-                    <p className={classes.name}>{course.name}</p>
-                    <p className={classes.section}>{course.section}</p>
+                    <li className={classes.point}></li>
+                    <div>
+                        <p className={classes.name}>{course.name}</p>
+                        <p className={classes.section}>{course.section}</p>
+                    </div>
                 </div>
             ))}
+            <section className={classes.join}>
+                <Button className={classes.joinBtn} startIcon={<PersonAddOutlinedIcon />} onClick={handleClickOpen}>
+                    Join A new class
+                </Button>
+            </section>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Join a course</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Enter the course code provided by your course instructor to join.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Enter code"
+                        type="email"
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleClose} color="primary">
+                        Join
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     )
 }
--- edchat/src/components/Courses/coursesStyle.js
+++ edchat/src/components/Courses/coursesStyle.js
@@ -1,19 +1,38 @@
 const coursesStyle = {
     courses: {
         marginLeft: '50px',
+
     },
     name: {
         fontSize: '12px',
         fontWeight: '600',
-        marginTop: '5px',
         lineHeight: '0.1',
 
     },
     section: {
         fontSize: '11px',
+
         color: '#8E8E8E',
         fontWeight: '400',
-        marginBottom: '30px',
+        marginBottom: '20px',
+
+    },
+    point: {
+        float: 'left',
+    },
+    joinBtn: {
+        textTransform: 'capitalize',
+        backgroundColor: '#B20838',
+        color: 'white',
+        boxShadow: '0px 5px 11px -8px rgba(0,0,0,0.86)',
+        borderRadius: '7px',
+        padding: '8px 30px',
+    },
+    join: {
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '5px'
+
     },
 };
 
