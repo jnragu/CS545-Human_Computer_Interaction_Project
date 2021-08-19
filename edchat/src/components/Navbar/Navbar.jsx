c2bd427064b2cf14dd13a7183b359396d87d148a
scalerjo <joseph.scalera1@gmail.com>
2021-08-19 15:44:53 +0000

    Revert "removed search bar"
    This reverts commit 4aa97f84ed3ee5d63d1a2f326e5487b828f6ec24.

--- edchat/src/components/Navbar/Navbar.jsx
+++ edchat/src/components/Navbar/Navbar.jsx
@@ -63,6 +63,18 @@ export default function Navbar() {
                         Stevens Institute of Technology
                     </Typography>
                 </Tooltip>
+                <TextField
+                    variant='outlined'
+                    label='Search'
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position='end'>
+                                <Search fontSize='small' />
+                            </InputAdornment>
+                        )
+                    }}
+                    className={classes.search}
+                />
                 <div className={classes.actions}>
                     <IconButton
                         onClick={handleClick}
